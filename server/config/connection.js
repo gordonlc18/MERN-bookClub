8e7f9a61c39034cc979545423c588cd85f685931
gordonlc18 <gordonlc18@gmail.com>
2022-06-06 22:12:42 -0400

    Fixing DB connect issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,39 @@
+// const { connect, connection } = require('mongoose');
+// const { config } = require('dotenv'); 
+
+// /*const {___} = require(<package>) is called Destructuring. 
+// This makes our code a lot more cleaner.
+// */
+
+// /*We'll use module.exports since we want to import this file in our server.js*/
+
+// module.exports = () => {
+//  config(); //invoking the dotenv config here
+//  const uri = process.env.DB_URI;
+
+//  connect(uri, {
+//         dbName: process.env.DB_NAME,
+//         user: process.env.DB_USER,
+//         pass: process.env.DB_PASSWORD,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false,
+//         useCreateIndex: true
+//     })
+//         .then(() => {
+//             console.log('📡Connection estabislished with MongoDB');
+//         })
+//         .catch(error => console.error(error.message));
+// }
+
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(`mongodb+srv://mern:yvxPPIRpqSbtu9Au@googlebooks.wklpdgc.mongodb.net/?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false,
+  useFindAndModify: true,
 });
 
 module.exports = mongoose.connection;

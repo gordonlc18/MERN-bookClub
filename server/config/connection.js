2febded7bb38a84ec833647c7a256250c43e0eca
gordonlc18 <gordonlc18@gmail.com>
2022-06-08 18:10:33 -0400

    ignore node_modules

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mern:Gjs2lCeKh2ThKkAd@social-network-api.xbxhz56.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
